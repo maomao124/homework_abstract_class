5db3b1922c152d7cc6352e9460ef3548e014cc33
毛 <1296193245@qq.com>
2021-11-03 20:39:43 +0800

    homework_abstract_class

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Project name(项目名称)：作业_abstract类
  * Package(包名): PACKAGE_NAME
@@ -17,5 +19,37 @@ public class test
         Manager e1 = new Manager("张三", 80000, "项目经理");
         e1.info();
         e1.work();
+
+        Scanner input = new Scanner(System.in);
+        Integer year = input.nextInt();
+        if ((year % 4 != 0) || (year % 100 == 0 && year % 400 != 0))
+        {
+            System.out.println("no");
+        }
+        else
+        {
+            System.out.println("yes");
+        }
+
+        input = new Scanner(System.in);
+        double x = input.nextDouble();
+        // 求 x 的绝对值。
+        /********* Begin *********/
+        if (x > 0)
+        {
+            System.out.println(x);
+        }
+        else if (x < 0)
+        {
+            System.out.println(-x);
+        }
+        else
+        {
+            System.out.println("ops");
+        }
+
+        /********* End *********/
+
+        input.close();
     }
 }
